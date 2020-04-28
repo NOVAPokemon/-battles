d1484eb6ea79ac638646032a9ee7612566953653
Bruno Anjos <banjos@localhost.localdomain>
2020-04-28 18:34:07 +0100

    refactor listen server

--- main.go
+++ main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/NOVAPokemon/utils"
 	"github.com/gorilla/websocket"
-	log "github.com/sirupsen/logrus"
-	"math/rand"
-	"net/http"
-	"time"
 )
 
-const host = utils.ServeHost
-const port = utils.BattlesPort
+const (
+	host = utils.ServeHost
+	port = utils.BattlesPort
+	serviceName = "BATTLES"
+)
 
-var addr = fmt.Sprintf("%s:%d", host, port)
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
 func main() {
-
-	rand.Seed(time.Now().Unix())
-	r := utils.NewRouter(routes)
-	log.Infof("Starting BATTLES server in port %d...\n", port)
-	log.Fatal(http.ListenAndServe(addr, r))
-
+	utils.CheckLogFlag(serviceName)
+	utils.StartServer(serviceName, host, port, routes)
 }
